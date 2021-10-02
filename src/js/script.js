3aa9926a573b74661173184a31502d39185b1575
Mkaell <mrohacevich@yandex.ru>
2021-10-02 15:53:02 +0300

    added function toggleMenu

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
         menuItem = document.querySelectorAll('.menu_item'),
-        hamburger = document.querySelector('.hamburger');
+        hamburger = document.querySelector('.hamburger'),
+        back = document.querySelector('.back');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
 
     menuItem.forEach(item => {
-        item.addEventListener('click', () => {
+        toggleMenu(item);
+
+    });
+
+
+    function toggleMenu(element){
+        element.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('menu_active');
-        })
-    })
-})
+            back.classList.toggle('show');
+        });
+    }
+
+    toggleMenu(hamburger);
+    toggleMenu(back);
+
+});
